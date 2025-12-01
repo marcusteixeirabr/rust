6a1f9963813fcfdebeb870c54f9ec95b6f8cc208
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-12-01 17:17:35 +0000

    Ajusta arquivos de configuração do conteiner.

--- meu_app/src/main.rs
+++ meu_app/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
+    let mut input = String::new();
+    println!("Digite alguma coisa");
+    let _ = io::stdin().read_line(&mut input)
+        //.expect("Falha ao ler a linha")
+        ;
+    //println!("Você digitou: {}", input);
+    println!("Você digitou: {input}");
+
+    let numero_secreto = rand::thread_rng().gen_range(1..=100);
+
+    println!("O número secreto é {numero_secreto}");
 }
